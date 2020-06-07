b961894c3697f5687b7a32342df134ba294eba19
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2020-06-07 20:32:07 +0530

    third commit

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import user
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(user,on_delete=models.CASCADE,null=True,blank=True);
+    name = models.CharField(max_length=200, null=True);
+    email = models.CharField(max_length=200, null=True);
+
+    def __str__(self):
+        return self.name
+
+class Product():
+        
