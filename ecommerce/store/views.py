cdb9cb56e8afe0f0cf5dae04e89747e867cf8e99
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2020-06-08 17:04:42 +0530

    fourth commit

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def store(request):
-    context={}
+    products = Product.objects.all()
+    context={'products':products}
     return render(request,'store/store.html',context)
 
 def checkout(request):
@@ -11,5 +12,11 @@ def checkout(request):
     return render(request,'store/checkout.html',context)
 
 def cart(request):
-    context={}
+    if request.user.is_aunthenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context={'items':items}
     return render(request,'store/cart.html',context)    
